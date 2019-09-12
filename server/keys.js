f18bb5ec48efe206c67b4c91960ef9f53f683f49
Anthony <aeghobor@gmail.com>
2019-09-12 23:55:56 +0100

    putting double quotes around strings in keys file for both worker and server

--- server/keys.js
+++ server/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
-  redisHost: redis.brbxsz.0001.euw1.cache.amazonaws.com,
+  redisHost: "redis.brbxsz.0001.euw1.cache.amazonaws.com",
   redisPort: 6379,
-  pgUser: pguser,
-  pgHost: pgdatabase.cpkxqc4m7ojs.eu-west-1.rds.amazonaws.com,
-  pgDatabase: pgdatabase,
-  pgPassword: pgpassword,
+  pgUser: "pguser",
+  pgHost: "pgdatabase.cpkxqc4m7ojs.eu-west-1.rds.amazonaws.com",
+  pgDatabase: "pgdatabase",
+  pgPassword: "pgpassword",
   pgPort: 5432
 };
